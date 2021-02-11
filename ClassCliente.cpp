60aa0a6381415c809856816c382d23e50f3c5b4c
jcondorio <78916447+jcondorio@users.noreply.github.com>
2021-02-11 10:16:32 -0500

    Create ClassCliente.cpp
    Avanze1ClassCliente

---
+++ ClassCliente.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include<stdlib.h>
+using namespace std;
+int main()
+{
+	class cliente
+	{
+		private:
+		String id_cliente;
+		String direccion;
+		String	telefono;
+		String email;
+		public:
+			modificarCliente();
+			altaCliente();
+			BajaCliente();
+			VisualizarCliente();
+	}
+}
