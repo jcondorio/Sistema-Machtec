068e1867abaa0fb54bdd5ebd7155b252a2e33a67
dhuallpar <78916666+dhuallpar@users.noreply.github.com>
2021-02-11 10:33:15 -0500

    Create Comprobante.h

---
+++ Comprobante.h
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+
+class Comprobante
+{
+		private:
+			string Nro_comprobante;
+			string Fecha;
+			string Nom_Cliente;
+			string Nom_Producto;
+			int Cantidad;
+			int Subtotal_Precio;
+			int Costo_Total
+			int igv;
+		public:
+			Persona(string);
+			int Cal_CostoTotal();
+			int Cal_Subtotal();
+			int Cal_igv();
+};
+
+Persona::Persona(string numc, string f, string nombc, string nombp, int c, int st, int ct, int i)
+{
+	Nro_comprobante=numc;
+	Fecha=f;
+	
+}
